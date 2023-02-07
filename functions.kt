41f641bc72465df3619260b0041b7ce39321c7b6
Kwibuka Confiance <86770870+kwibuka-confiance@users.noreply.github.com>
2023-02-07 12:37:24 +0200

    adding lambda and inline functions

--- functions.kt
+++ functions.kt
@@ -1,10 +1,21 @@
 fun main() {
     val calculate = calculate(12, 34, ::sum)
     println(calculate)
+    print(upperCase("Hello Motion"))
+    myFunction({ println("Hello motion") })
 }
 
 fun sum(num1: Int, num2: Int) = num1 + num2
 
 fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
     return operation(x, y)
+}
+// Lambda Function
+var upperCase = { str: String -> str.toUpperCase() }
+
+// Inline Function
+inline fun myFunction(function: () -> Unit) {
+    println("Am inline function -A")
+    function()
+    println("Am inline function -B")
 }
