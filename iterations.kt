2f2a64cb7107c12773f6b74ceed44beff88e0c73
Kwibuka Confiance <86770870+kwibuka-confiance@users.noreply.github.com>
2023-02-07 15:10:32 +0200

    adding iteration

---
+++ iterations.kt
@@ -0,0 +1,37 @@
+fun main() {
+    // var num = 1
+
+    // while (num <= 20) {
+    //     num += 1
+    //     print(num)
+    // }
+
+    // var number = 9
+    // var factorial = 1
+
+    // do {
+    //     factorial *= number
+    //     number--
+    // } while (number > 0)
+
+    // println("The factorial of 9 is $factorial")
+
+    // var colors = arrayOf<String>("Yellow", "Oranges", "Green", "Cyan")
+
+    // for ((index, value) in colors.withIndex()) {
+    //     println("value is $value and index is $index")
+    // }
+
+    // for (item in colors.indices) {
+    //     // println("colors[item]")
+    //     println("color index is $item and Color it self is ${colors[item]} ")
+    // }
+
+    val motion: String = "Moon"
+
+    when (motion) {
+        "Sun" -> println("The Sun is a Saterite")
+        "Moon" -> println("Moon is also a star")
+        "Earth" -> println("Earth is also a planet")
+    }
+}
