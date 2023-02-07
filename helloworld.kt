670941143ea1dbee71d2790131a727014af6b5e8
Kwibuka Confiance <86770870+kwibuka-confiance@users.noreply.github.com>
2023-02-07 11:39:52 +0200

    filter

--- helloworld.kt
+++ helloworld.kt
@@ -1,7 +1,13 @@
 fun main() {
-    for (num in 1 until 10) {
-        println(num)
-    }
+    // for (num in 1 until 10) {
+    //     println(num)
+    // }
+
+    // filter
+    val x = 1..10
+    val filteredNumber = x.filter { number -> number % 2 == 0 }
+
+    println(filteredNumber)
 }
 
 fun multiplication(num1: Int, num2: Int): Int {
