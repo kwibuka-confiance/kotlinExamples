68102e09aa7e9577c34eb6577d50df759170aa72
Kwibuka Confiance <86770870+kwibuka-confiance@users.noreply.github.com>
2023-02-09 23:06:09 +0200

    adding encapslation

--- unlabeledBreaks.kt
+++ unlabeledBreaks.kt
@@ -1,9 +1,17 @@
 fun main() {
-    try {
-        var num: Int = 50 / 0
-        println("Beginers ")
-        println(num)
-    } catch (c: Exception) {
-        println("It is not allowed to divide number by zero")
+    // try {
+    //     var num: Int = 50 / 0
+    //     println("Beginers ")
+    //     println(num)
+    // } catch (c: Exception) {
+    //     println("It is not allowed to divide number by zero")
+    // }
+    ConstructorClass(12, 34)
+}
+
+class ConstructorClass {
+    constructor(x: Int, y: Int) {
+        var area = x * y
+        println(area)
     }
 }
