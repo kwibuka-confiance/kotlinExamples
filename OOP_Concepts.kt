2413ba9e4c402c210b48503892ce2f9e7a2d7a60
Kwibuka Confiance <86770870+kwibuka-confiance@users.noreply.github.com>
2023-02-09 16:41:08 +0200

    adding encapslation

--- OOP_Concepts.kt
+++ OOP_Concepts.kt
@@ -1,9 +1,26 @@
 fun main() {
     // Employee().insertValue("Kwibuka", 23, 'M', 460600.4)
-    OuterClass.InnerClass().concantine("Confiance")
+    // OuterClass.InnerClass().concantine("Confiance")
+    // var company = Company()
+    // company.name = "Swift M"
+    // print(company.name)
+
+    // var y = Person()
+    // y.fullName = "swift motion"
+
+    // println(y.firstname)
+    // println(y.lastname)
+
+    val store = Store()
+    store.products.add(Product("Nike Shoes", 23.23))
+    store.products.add(Product("Adiddas Shoes", 69.23))
+    store.products.add(Product("Timber Shoes", 100.23))
+
+    val total_price = store.totalPrice
+    println(total_price)
 }
 
-class Employee {
+class employee {
     var name: String = ""
     var age: Int = 0
     var gender: Char = 'M'
@@ -31,4 +48,37 @@ class OuterClass {
             println(fullname)
         }
     }
+}
+
+// GETTERS AND SETTERS
+class Company {
+    var name: String = "Swift Motion"
+        get() = field
+        set(value) {
+            field = value
+        }
+}
+
+// class Person {
+//     var firstname: String = ""
+//     var lastname: String = ""
+//     var fullName: String
+//         get() = "$firstname $lastname"
+//         set(value) {
+//             var names = value.split(" ")
+//             firstname = names[0].toUpperCase()
+//             lastname = names[1].capitalize()
+//         }
+
+//         private set
+//         fun motion(){}
+// }
+
+class Product(val name: String, var price: Double)
+
+class Store {
+    var products = mutableListOf<Product>()
+
+    val totalPrice: Double
+        get() = products.sumByDouble { it.price }
 }
